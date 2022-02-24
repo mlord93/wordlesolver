3c114a479ed7389f4914f6654e78b7eb39a7aed9
Maddie Lord <maddielord@maddies-air.lan>
2022-02-23 22:44:12 -0800

    First Wordle Solver commit

---
+++ wordle.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+let words = getInput();
+
+function getInput() {
+    try {
+        const filePathString = `./words/AllWords.txt`
+        return fs.readFileSync(filePathString, 'utf8').split('\n');
+    } catch (e) {
+        throw e;
+    }
+}
+
+function play() {
+    while (true) {
+        let guess = prompt('Guess: ');
+        let result = prompt('Result: ');
+        if (result == 'ggggg') {
+            console.log(`Solution: ${guess}`);
+            break;
+        }
+        let forbiddenChars = result.split('').reduce((a, c, i) => c == 'b' ? [...a, guess[i]] : a, []);
+        words = words.filter(word => {
+            let usedIndexes = result.split('').reduce((a, c, i) => c == 'g' ? [...a, i] : a, []);
+            for (let i = 0; i<word.length; i++) {
+                if (forbiddenChars.includes(word[i])) return false; 
+                if (result[i] == 'g' && guess[i] !== word[i] ) return false;
+                if (result[i] == 'y' ) {
+                    if (guess[i] == word[i]) return false;
+                    let unusedMatches = getAllOccurances(word, guess[i]).filter(c => !usedIndexes.includes(c))
+                    if (unusedMatches.length == 0) return false;
+                    usedIndexes.push(unusedMatches[0]);
+                }
+            }
+            return true;
+        });
+        console.log(words);
+    }
+}
+
+function getAllOccurances(word, char) {
+    const occurances = [];
+    for (let i = 0; i<word.length; i++) {
+        if (word[i] == char) occurances.push(i);
+    }
+    return occurances;
+}
+
+play();
